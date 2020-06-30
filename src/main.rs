d993dc694fead0348020b64e579a6962c09afc41
Michael Madden <mikemadden42@icloud.com>
2020-06-30 14:25:30 -0500

    Update start date

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let utc = Utc;
     let d1 = Utc::now();
     let d2 = utc
-        .datetime_from_str(&"Apr 8 08:00:00 2019", "%b %d %H:%M:%S %Y")
+        .datetime_from_str(&"Jul 6 08:00:00 2020", "%b %d %H:%M:%S %Y")
         .unwrap();
     let duration = d2.signed_duration_since(d1);
     println!("diff: {:?} days", duration.num_days().abs());
