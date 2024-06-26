1857d20766fe9adc2d4c98147c0f255a6956c6b8
Michael Madden <mikemadden42@icloud.com>
2024-06-25 21:20:55 -0500

    Fix deprecation.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use chrono::offset::{TimeZone, Utc};
+use chrono::NaiveDateTime;
 
 fn main() {
-    let utc = Utc;
     let d1 = Utc::now();
-    let d2 = utc
-        .datetime_from_str("Jul 6 08:00:00 2020", "%b %d %H:%M:%S %Y")
-        .unwrap();
+    let naive_d2 =
+        NaiveDateTime::parse_from_str("Jul 6 08:00:00 2020", "%b %d %H:%M:%S %Y").unwrap();
+    let d2 = Utc.from_utc_datetime(&naive_d2);
     let duration = d2.signed_duration_since(d1);
     println!("diff: {:?} days", duration.num_days().abs());
 }
